ec1a9c9903ad00a2608ed28fd5c52e4f6690633d
yeongun130 <yeongunj625@gmail.com>
2026-09-17 15:28:51 +0900

    deploy

--- service-worker.js
+++ service-worker.js
@@ -72,7 +72,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
     self.addEventListener('install', function (e) {
         console.debug('[ServiceWorker] Installing offline worker');
         e.waitUntil(
-            caches.open('1fa86cbd-08f9-4103-aeab-d3826391851b').then(async function (cache) {
+            caches.open('9b680533-354d-4abd-b559-98c274e2c0e5').then(async function (cache) {
                 console.debug('[ServiceWorker] Caching app binaries and content');
 
                 await cacheFilesWithConcurrency(cache, unoConfig.offline_files, MAX_CACHE_CONCURRENCY);
@@ -150,7 +150,7 @@ if (unoConfig.environmentVariables["UNO_BOOTSTRAP_DEBUGGER_ENABLED"] !== "True")
             caches.keys().then(function (cacheNames) {
                 return Promise.all(
                     cacheNames.filter(function (cacheName) {
-                        return cacheName !== '1fa86cbd-08f9-4103-aeab-d3826391851b';
+                        return cacheName !== '9b680533-354d-4abd-b559-98c274e2c0e5';
                     }).map(function (cacheName) {
                         console.debug('[ServiceWorker] Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
